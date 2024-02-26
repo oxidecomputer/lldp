f96375c97622e2bba113f1e12f7634b7f82fa0da
Nils Nieuwejaar <nils@oxidecomputer.com>
2024-02-26 16:35:03 +0000

    add license to each file

--- lldpd-client/src/lib.rs
+++ lldpd-client/src/lib.rs
@@ -2,6 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 // Copyright 2024 Oxide Computer Company
 
 //! Client library for the LLDPS daemon.
